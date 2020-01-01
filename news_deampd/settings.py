ebfbc5e6a3a5f4d70d0e42f03e62ac55d54dea79
Marc LaBelle <mjlabe@gmail.com>
2019-12-31 21:38:43 -0500

    debug False

--- news_deampd/settings.py
+++ news_deampd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#orxj#dno5$rat6tun$+rq!ro2l*fc@vejrrk%#50m%=3a2uc3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
