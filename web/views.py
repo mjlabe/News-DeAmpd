11a487435de3e96c58d1b616d7b13dec4b4968c3
Marc LaBelle <mlabelle@acvauctions.com>
2020-10-02 15:36:39 -0400

    add newscatcher source

--- web/views.py
+++ web/views.py
@@ -1,7 +1,10 @@
-from GoogleNews import GoogleNews
 from django.shortcuts import render
 from django.views import View
 from django.views.decorators.cache import cache_page
+
+from newscatcher import Newscatcher
+from GoogleNews import GoogleNews
+
 
 from news_deampd import settings
 
@@ -15,3 +18,21 @@ class NewsViewSet(View):
         news = GoogleNews()
         news.get_news(deamplify=True)
         return render(request, 'news.html', {'data': news.result()})
+
+
+class NewscatcherView(View):
+    news = {}
+    websites = []
+    topics = ['news', 'tech',  'science', 'world', 'business', 'finance', 'politics', 'economics', 'travel',]
+
+    @cache_page(CACHE_TTL)
+    def get(self, request):
+        return render(request, 'news.html', {'data': self.news})
+
+    def get_news(self):
+        for topic in self.topics:
+            for website in self.websites:
+                nc = Newscatcher(website=website, topic=topic)
+
+                results = nc.get_news()
+                self.news[topic] = results['articles'][:10]
